81d815925be612f364a12ca05c487edf33ef54dd
Santjaga84 <Santjaga84@gmail.com>
2022-03-15 22:41:53 +0200

    Source control

--- js/main.js
+++ js/main.js
@@ -22,3 +22,11 @@ var reviewsSlider = new Swiper(".reviews-slider", {
   },
   
 });
+
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener('click', function () {
+  console.log('клик по кнопке меню');
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
+});
